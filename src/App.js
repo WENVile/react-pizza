a5c0aec54498f52160e578190f9ffabe89bd1fdd
Alexandr Ogurthov <oleksandr.ohurtsov@gmail.com>
2023-04-17 16:18:44 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from './Components/Categories';
+import Sort from './Components/Sort';
+import Header from './Components/Header';
+import './scss/app.scss';
+import PizzaBlock from './Components/PizzaBlock';
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="wrapper">
+			<Header />
+
+			<div className="content">
+				<div className="container">
+					<div className="content__top">
+						<Categories />
+
+						<Sort />
+					</div>
+					<h2 className="content__title">Всі піцци</h2>
+					<div className="content__items">
+						<PizzaBlock title="Pizza 1" price={300} />
+						<PizzaBlock title="Pizza 2" price={400} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/Components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Categories() {
+	return (
+		<div className="categories">
+			<ul>
+				<li className="active">Всі</li>
+				<li>М'ясні</li>
+				<li>Вегетеріанські</li>
+				<li>Гриль</li>
+				<li>Гострі</li>
+				<li>Закриті</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Categories;
