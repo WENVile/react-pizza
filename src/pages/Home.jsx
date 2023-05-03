73bf6e2c82d6acb79eeab7c31961206eb205c3e4
Alexandr Ogurthov <oleksandr.ohurtsov@gmail.com>
2023-05-03 23:39:13 +0300

    ///

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,8 +38,8 @@ function Home() {
 			</div>
 			<h2 className="content__title">Всі Піци</h2>
 			<div className="content__items">
-				{items.map((el) => {
-					return <PizzaBlock key={el.uniqueId} {...el} />;
+				{items.map((el, index) => {
+					return <PizzaBlock key={index} {...el} />;
 				})}
 			</div>
 		</div>
